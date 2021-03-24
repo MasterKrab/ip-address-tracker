acd129858b43e1e440c595f8341c285db4dc2d56
Mrs. Krabs <47456460+MasterKrab@users.noreply.github.com>
2021-03-24 17:14:07 -0300

    Update

--- src/js/b_functions.js
+++ src/js/b_functions.js
@@ -11,36 +11,41 @@ const showMap = (lat, lng) =>{
    marker = L.marker([lat, lng], {icon: myIcon}).addTo(mymap);
 }
 
-const getLocationData = (ip) =>{
-   const url = `https://geo.ipify.org/api/v1?apiKey=${apiKey}&ipAddress=${ip}`;
+const getLocationData = async (search, type="ipAddress") =>{
+   const url = `https://geo.ipify.org/api/v1?apiKey=${apiKey}&${type}=${search}`;
+   console.log(url);
 
-   fetch(url)
-      .then(response => response.json())
-      .then(data => {
-         mymap.setView([data.location.lat, data.location.lng], 13);
-         marker.remove();
-         marker = L.marker([data.location.lat, data.location.lng], {icon: myIcon}).addTo(mymap);
+   const request = await fetch(url);
+   const data = await request.json();
 
-         ipElement.textContent = data.ip;
-         locationElement.textContent = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`;
-         timezoneElement.textContent = `UTC ${data.location.timezone}`;
-         providerElement.textContent = data.isp;
-      })
-      .catch((error) =>{
-         console.error(error);
-      });
+   mymap.setView([data.location.lat, data.location.lng], 13);
+   marker.remove();
+   marker = L.marker([data.location.lat, data.location.lng], {icon: myIcon}).addTo(mymap);
+
+   ipElement.textContent = data.ip;
+   locationElement.textContent = `${data.location.city}, ${data.location.region} ${data.locatiopostalCode}`;
+   timezoneElement.textContent = `UTC ${data.location.timezone}`;
+   providerElement.textContent = data.isp;
 }
 
-const getCurrentLocationData = () =>{
+const getCurrentLocationData = async () =>{
    const url = `https://geo.ipify.org/api/v1?apiKey=${apiKey}`;
-   
-   fetch(url)
-      .then(response => response.json())
-      .then(data => {
-         showMap(data.location.lat, data.location.lng);
-         ipElement.textContent = data.ip;
-         locationElement.textContent = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`;
-         timezoneElement.textContent = `UTC ${data.location.timezone}`;
-         providerElement.textContent = data.isp;
-      })
+   const request = await fetch(url);
+   const data = await request.json();
+
+   showMap(data.location.lat, data.location.lng);
+   ipElement.textContent = data.ip;
+   locationElement.textContent = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`;
+   timezoneElement.textContent = `UTC ${data.location.timezone}`;
+   providerElement.textContent = data.isp;     
+}
+
+const validateUrl = (url) => {
+   const urlRegex = /(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]/g
+   return urlRegex.test(url) ? true : false;
+}
+
+const validateIP = (ip) => {
+   const ipRegex = /((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))/g;
+   return ipRegex.test(ip) ? true : false
 }
--- src/js/c_eventlisteners.js
+++ src/js/c_eventlisteners.js
@@ -1,6 +1,16 @@
 form.addEventListener("submit", e => {
    e.preventDefault();
-   form.ip.validity.valid && getLocationData(form.ip.value);
+
+   if(validateIP(form.search.value)){
+      getLocationData(form.search.value);
+      return;
+   }
+   
+   if(validateUrl(form.search.value)){
+      const url = form.search.value.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n\?\=]+)/im);
+      getLocationData(url[1], "domain");
+      return;
+   };
 });
 
 document.addEventListener("DOMContentLoaded", getCurrentLocationData);
